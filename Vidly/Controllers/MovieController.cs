89161f9544d324345c823a04cc1a1987acce32dd
Luka Bulatovic <luka.dev94@gmail.com>
2019-05-24 14:10:09 +0200

    ViewModels, Partials

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Kill Bill" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Luka" },
+                new Customer { Name = "Sanja" }
+            };
+
+            var viewModel = new RandomMovieViewModel { Movie = movie, Customers = customers };
+
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("year={0}, month={1}", year, month));
+        }
+    }
+}
